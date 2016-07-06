35d0b78ee558632a04b758b5730ca6d5b45829c5
jekakmail <jekakmail@gmail.com>
2016-07-06 21:00:56 +0300

    Added UI prototype Added map control

--- LondonTransport/Dashboard.aspx.cs
+++ LondonTransport/Dashboard.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Subgurim.Controles;
 
 namespace LondonTransport
 {
@@ -11,7 +12,11 @@ namespace LondonTransport
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            GMap1.setCenter(new GLatLng(37.339085, -121.8914807), 18);
+            GMap1.mapType = GMapType.GTypes.Hybrid;
+            GMap1.Add(GMapType.GTypes.Physical);
+            GMap1.Add(new GControl(GControl.preBuilt.MapTypeControl));
+            GMap1.enableRotation = true;
         }
     }
 }
--- LondonTransport/Dashboard.aspx.designer.cs
+++ LondonTransport/Dashboard.aspx.designer.cs
@@ -11,5 +11,14 @@ namespace LondonTransport {
     
     
     public partial class WebForm1 {
+        
+        /// <summary>
+        /// GMap1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::Subgurim.Controles.GMap GMap1;
     }
 }
